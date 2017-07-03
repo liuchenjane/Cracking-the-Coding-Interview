f4c3f534fd430f66612711595fb32757799dc626
liuchenjane <2386418764@qq.com>
2017-07-03 08:33:37 +0800

    Update checkDifferent.cpp

--- checkDifferent.cpp
+++ checkDifferent.cpp
@@ -1,3 +1,4 @@
+//解1：
 class Different {
 public:
     bool checkDifferent(string iniString) {
@@ -11,6 +12,39 @@ public:
             int x=iniString[i];
             hash[x]++;
             if(hash[x]>1)
+                return false;
+        }
+        return true;
+    }
+};
+//解2：
+class Different {
+public:
+    bool checkDifferent(string iniString) {
+        // write code here
+        int n=iniString.size();
+        if(n>256) return false;
+       
+        for (int i=0; i<n; i++){
+            for (int j=0;j<n;j++){
+                if(i!=j && (iniString[i] == iniString[j]))
+                    return false;
+            }
+        }
+        return true;
+    }
+};
+//解3：
+class Different {
+public:
+    bool checkDifferent(string iniString) {
+        // write code here
+        int n=iniString.size();
+        if(n>256) return false;
+         
+        sort(iniString.begin(),iniString.end());
+        for (int i=0; i<n-1; i++){
+            if(iniString[i] == iniString[i+1])
                 return false;
         }
         return true;
